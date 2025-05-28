11ae45eebd5be3a3619c38c0aa58f7b163590a0d
TemuulenBattulgaGit <TemuulenBattulga1209@gmail.com>
2025-05-28 02:02:50 -0700

    setting up to start on lexer function

--- compiler_project/src/main.rs
+++ compiler_project/src/main.rs
@@ -13,6 +13,71 @@ enum Token {
     End,
 }
 
+fn create_identifier(code: &str) -> Token {
+  match code {
+  "func" => Token::Func,
+  "return" => Token::Return,
+  "int" => Token::Int,
+
+  // todo: implement all keywords...
+  // ... all keywords...
+
+  "read" => Token::Read,
+  "while" => Token::While,
+  "if" => Token::If,
+  _ => Token::Ident(String::from(code)),
+  }
+}
+
+// This is a lexer that parses numbers and math operations
+fn lex(mut code: &str) -> Result<Vec<Token>, String> {
+  let bytes = code.as_bytes();
+  let mut tokens: Vec<Token> = vec![];
+
+  let mut i = 0;
+  while i < bytes.len() {
+    let c = bytes[i] as char;
+
+    match c {
+
+    '0'..='9' => {
+      let start = i;
+      i += 1;
+      while i < bytes.len() {
+        let digit = bytes[i] as char;
+        if digit >= '0' && digit <= '9' {
+          i += 1;
+        } else {
+          break;
+        }
+      }
+      let end = i;
+      let string_token = &code[start..end];
+      let number_value = string_token.parse::<i32>().unwrap();
+      let token = Token::Num(number_value);
+      tokens.push(token);
+    }
+
+    '+' => {
+      tokens.push(Token::Plus);
+      i += 1;
+    }
+
+    ' ' | '\n' => {
+      i += 1;
+    }
+
+    _ => {
+      return Err(format!("Unrecognized symbol '{}'", c));
+    }
+
+    }
+  }
+
+  tokens.push(Token::End);
+  return Ok(tokens);
+}
+
 
 fn main() {
 
