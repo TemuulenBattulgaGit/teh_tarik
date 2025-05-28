1c71cdcfb36360a74a34f26e743e5150fc7a60d2
TemuulenBattulgaGit <TemuulenBattulga1209@gmail.com>
2025-05-27 22:39:41 -0700

    created "enum Token" to identify the list of tokens

--- compiler_project/src/main.rs
+++ compiler_project/src/main.rs
@@ -1,3 +1,40 @@
 fn main() {
-    println!("Hello, world!");
+
+    //First setting up my enum Token
+    #[derive(Debug, Clone)]
+    enum Token {
+    Func,
+    Return,
+    Int,
+    Print,
+    Read,
+    While,
+    If,
+    Else,
+    Break,
+    Continue,
+    LeftParen,
+    RightParen,
+    LeftCurly,
+    RightCurly,
+    LeftBraket,
+    RightBraket,
+    Comma,
+    Semicolon,
+    Plus,
+    Substract,
+    Multiply,
+    Devide,
+    Modulus,
+    Assign,
+    Less,
+    LessEqual,
+    Greater,
+    GreaterEqual,
+    Equality,
+    NotEqual,
+    Ident(String),
+    Num(i32)
+    End,
+    }
 }
